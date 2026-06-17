8f64b86fb4c1fdb67e4ff1517ce04015c8740929
Mohammadreza Sufi <55635079+SufiM@users.noreply.github.com>
2026-06-18 01:19:45 +0330

    Update formatter

--- pricebot/services/formatter.py
+++ pricebot/services/formatter.py
@@ -4,6 +4,7 @@ def format_prices(prices):
 
     return (
         "**  قیمت های لحظه ای  **\n\n"
+        "  قیمت ها به تومان می باشد  \n\n"
         f" طلا:  {clean(prices['gold'])}\n"
         f" دلار:  {clean(prices['usd'])}\n"
         f" یورو:  {clean(prices['eur'])}\n"
